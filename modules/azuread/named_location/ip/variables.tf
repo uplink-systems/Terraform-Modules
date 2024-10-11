270194da253b66a479892aa5fa316fc0118481c4
andreasschreiner-uplinksystems <167195902+andreasschreiner-uplinksystems@users.noreply.github.com>
2024-10-11 13:01:56 +0200

    Delete modules/azuread/named_location/ip directory

--- modules/azuread/named_location/ip/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "display_name" {
-  description = "Named location's display name"
-}
-variable "ip_ranges" {
-  description = "IP/subnet for named location"
-}
-variable "trusted" {
-  description = "Add ip/subnet to trusted IPs?"
-}
