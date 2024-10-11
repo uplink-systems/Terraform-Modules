70514039181218269f07cafc419955438e119292
andreasschreiner-uplinksystems <167195902+andreasschreiner-uplinksystems@users.noreply.github.com>
2024-10-11 13:01:47 +0200

    Delete modules/azuread/named_location/country directory

--- modules/azuread/named_location/country/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "display_name" {
-  description = "Named location's display name"
-}
-variable "countries_and_regions" {
-  description = "Code(s) for countries and regions"
-}
-variable "include_unknown_countries_and_regions" {
-  description = "Treat unknown countries and regions as location-included?"
-}
