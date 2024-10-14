ea49921dd7dd38392de215a2b57fe1e9e3b7c16b
andreasschreiner-uplinksystems <167195902+andreasschreiner-uplinksystems@users.noreply.github.com>
2024-10-14 12:27:34 +0200

    Update versions.tf

--- examples/azurerm/dns/versions.tf
+++ examples/azurerm/dns/versions.tf
@@ -3,7 +3,7 @@
 ####################################################################################################
 
 terraform {
-  required_version = "~> 1.7.0"
+  required_version = "~> 1.9.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
