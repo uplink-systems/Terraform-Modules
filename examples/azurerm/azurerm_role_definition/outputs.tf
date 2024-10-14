396b222c1d46b15d8c5ef3091718b004fc7be04e
andreasschreiner-uplinksystems <167195902+andreasschreiner-uplinksystems@users.noreply.github.com>
2024-10-14 12:59:15 +0200

    Add files via upload

---
+++ examples/azurerm/azurerm_role_definition/outputs.tf
@@ -0,0 +1,13 @@
+####################################################################################################
+#   outputs.tf                                                                                     #
+####################################################################################################
+
+output "azurerm_role_definition" {
+    value         = azurerm_role_definition.role_definition[*]
+    description   = "List of all exported attributes values from all azurerm_role_definition resources"
+}
+
+output "azurerm_role_definition_role_definition_id" {
+    value         = azurerm_role_definition.role_definition[*].role_definition_id
+    description   = "List of role_definitions_id attribute values from zurerm_role_definition resources"
+}
