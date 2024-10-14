7adf1de1356543929ec6c5f9f0bf131067cb388f
andreasschreiner-uplinksystems <167195902+andreasschreiner-uplinksystems@users.noreply.github.com>
2024-10-14 15:20:12 +0200

    Update locals.tf

--- modules/azuread/group/locals.tf
+++ modules/azuread/group/locals.tf
@@ -2,9 +2,9 @@
 #   locals.tf                                                                                      #
 ####################################################################################################
 
-# This is used to build the group's member list containing member user ids and member group ids...
+# This is used to build the group's member list containing member user ids, service principal ids and member group ids...
 locals {
-  members = concat(data.azuread_users.members.object_ids,data.azuread_groups.members.object_ids)
+  members = concat(data.azuread_users.members.object_ids,data.azuread_groups.members.object_ids,data.azuread_service_principals.members.object_ids)
 }
 
 # This is used to buld the administrative units list containing administrative unit's object ids...
