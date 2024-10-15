18566318fa649073c93a3a990f1234c596659dfa
andreasschreiner-uplinksystems <167195902+andreasschreiner-uplinksystems@users.noreply.github.com>
2024-10-15 09:37:01 +0200

    Update outputs.tf

--- modules/azuread/group/outputs.tf
+++ modules/azuread/group/outputs.tf
@@ -3,16 +3,16 @@
 ####################################################################################################
 
 output "azuread_group" {
-    value         = azuread_group.group[*]
+    value         = azuread_group.group
     description   = "List of all exported attributes values from all azuread_group resources"
 }
 
-output "azuread_group_display_names" {
-    value         = azuread_group.group[*].display_name
+output "azuread_group_display_name" {
+    value         = azuread_group.group.display_name
     description   = "List of display_name attribute values from azuread_group resources"
 }
 
-output "azuread_group_object_ids" {
-    value         = azuread_group.group[*].object_id
+output "azuread_group_object_id" {
+    value         = azuread_group.group.object_id
     description   = "List of object_id attribute values from azuread_group resources"
 }
