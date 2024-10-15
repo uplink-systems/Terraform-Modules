96595ac38073cfc76abf1e5213e1d1a5c1586dc2
andreasschreiner-uplinksystems <167195902+andreasschreiner-uplinksystems@users.noreply.github.com>
2024-10-15 08:44:45 +0200

    Create outputs.tf

---
+++ modules/azurerm/dns/outputs.tf
@@ -0,0 +1,118 @@
+####################################################################################################
+#   outputs.tf                                                                                     #
+####################################################################################################
+
+output "azurerm_dns_zone" {
+  value       = azurerm_dns_zone.zone
+  description = "List of all exported attributes values from all DNS zones"
+}
+
+output "azurerm_dns_zone_id" {
+  value       = azurerm_dns_zone.zone.id
+  description = "List of exported 'id' attribute values from all DNS zones"
+}
+
+output "azurerm_dns_zone_name" {
+  value       = azurerm_dns_zone.zone.name
+  description = "List of exported 'name' attribute values from all DNS zones"
+}
+
+output "azurerm_dns_zone_name_servers" {
+  value       = azurerm_dns_zone.zone.name_servers
+  description = "List of exported 'name_servers' attribute values from all DNS zones"
+}
+
+output "azurerm_dns_zone_number_of_record_sets" {
+  value       = azurerm_dns_zone.zone.number_of_record_sets
+  description = "List of exported 'number_of_record_sets' attribute values from all DNS zones"
+}
+
+output "azurerm_dns_a_record" {
+  value       = azurerm_dns_a_record.recordset
+  description = "List of all exported attributes values from all DNS A record sets"
+}
+
+output "azurerm_dns_aaaa_record" {
+  value       = azurerm_dns_aaaa_record.recordset
+  description = "List of all exported attributes values from all DNS AAA record sets"
+}
+
+output "azurerm_dns_caa_record" {
+  value       = azurerm_dns_caa_record.recordset
+  description = "List of all exported attributes values from all DNS CAA record sets"
+}
+
+output "azurerm_dns_cname_record" {
+  value       = azurerm_dns_cname_record.recordset
+  description = "List of all exported attributes values from all DNS CNAME record sets"
+}
+
+output "azurerm_dns_mx_record" {
+  value       = azurerm_dns_mx_record.recordset
+  description = "List of all exported attributes values from all DNS MX record sets"
+}
+
+output "azurerm_dns_ns_record" {
+  value       = concat(data.azurerm_dns_ns_record.recordset,azurerm_dns_ns_record.recordset)
+  description = "List of all exported attributes values from all DNS NS root record sets"
+}
+
+output "azurerm_dns_soa_record" {
+  value       = data.azurerm_dns_soa_record.recordset
+  description = "List of all exported attributes values from all DNS SOA record sets"
+}
+
+output "azurerm_dns_srv_record" {
+  value       = azurerm_dns_srv_record.recordset
+  description = "List of all exported attributes values from all DNS SRV record sets"
+}
+
+output "azurerm_dns_txt_record" {
+  value       = azurerm_dns_a_record.recordset
+  description = "List of all exported attributes values from all DNS TXT record sets"
+}
+
+output "azurerm_dns_a_record_id" {
+  value       = azurerm_dns_a_record.recordset.id
+  description = "List of exported id attribute values from all DNS A record sets"
+}
+
+output "azurerm_dns_aaaa_record_id" {
+  value       = azurerm_dns_aaaa_record.recordset.id
+  description = "List of exported id attribute values from all DNS AAA record sets"
+}
+
+output "azurerm_dns_caa_record_id" {
+  value       = azurerm_dns_caa_record.recordset.id
+  description = "List of exported id attribute values from all DNS CAA record sets"
+}
+
+output "azurerm_dns_cname_record_id" {
+  value       = azurerm_dns_cname_record.recordset.id
+  description = "List of exported id attribute values from all DNS CNAME record sets"
+}
+
+output "azurerm_dns_mx_record_id" {
+  value       = azurerm_dns_mx_record.recordset.id
+  description = "List of exported id attribute values from all DNS MX record sets"
+}
+
+output "azurerm_dns_ns_record_id" {
+  value       = concat(data.azurerm_dns_ns_record.recordset.id,azurerm_dns_ns_record.recordset.id)
+  description = "List of exported id attribute values from all DNS NS root record sets"
+}
+
+output "azurerm_dns_soa_record_id" {
+  value       = data.azurerm_dns_soa_record.recordset.id
+  description = "List of exported id attribute values from all DNS SOA record sets"
+}
+
+output "azurerm_dns_srv_record_id" {
+  value       = azurerm_dns_srv_record.recordset.id
+  description = "List of exported id attribute values from all DNS SRV record sets"
+}
+
+output "azurerm_dns_txt_record_id" {
+  value       = azurerm_dns_a_record.recordset.id
+  description = "List of exported id attribute values from all DNS TXT record sets"
+}
