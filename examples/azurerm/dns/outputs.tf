3efb7a7a590fb16589c7b29ef9d0cfceca6cb561
andreasschreiner-uplinksystems <167195902+andreasschreiner-uplinksystems@users.noreply.github.com>
2024-10-15 09:29:34 +0200

    Create outputs.tf

---
+++ examples/azurerm/dns/outputs.tf
@@ -0,0 +1,11 @@
+####################################################################################################
+#   outputs.tf                                                                                     #
+####################################################################################################
+
+output "azurerm_dns_zone_ids" {
+    value = values(module.azurerm_dns).*.azurerm_dns_zone_id
+}
+
+output "azurerm_dns_zone_ids_toset" {
+    value = toset([ for object_id in module.azurerm_dns : object_id.azurerm_dns_zone_id ])
+}
