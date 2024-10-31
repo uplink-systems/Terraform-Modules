0dfbf5c0f8137b06c98bf2f70bff7637782a444b
Andreas Schreiner <andreas.schreiner@uplink.systems>
2024-10-31 12:15:55 +0100

    updated azuread_inviation module

--- modules/azuread/invitation/main.tf
+++ modules/azuread/invitation/main.tf
@@ -1,17 +1,20 @@
 ####################################################################################################
-#   Azure AD Invitations (Guest users)                                                             # 
+#   main.tf                                                                                        # 
 ####################################################################################################
 
 resource "azuread_invitation" "invitation" {
-  user_display_name   = var.user_display_name
-  user_email_address  = var.user_email_address
-  message {
-    additional_recipients = var.message.additional_recipients
-    body                  = var.message.body
-    language              = var.message.body == null ? var.message.language : null
+  user_display_name   = var.invitation.user_display_name
+  user_email_address  = var.invitation.user_email_address
+  dynamic "message" {
+    for_each          = var.invitation.message.enabled ? [1] : []
+    content {
+      additional_recipients = var.invitation.message.additional_recipients
+      body                  = var.invitation.message.body
+      language              = var.invitation.message.body == null ? var.invitation.message.language : null
+    }
   }
-  redirect_url        = var.redirect_url
-  user_type           = var.user_type
+  redirect_url        = var.invitation.redirect_url
+  user_type           = var.invitation.user_type
   lifecycle {
     ignore_changes    = [ message ]
   }
--- modules/azuread/invitation/variable.tf
+++ modules/azuread/invitation/variable.tf
@@ -1,29 +1,39 @@
 ####################################################################################################
-#   variables.tf                                                                                   #
+#   variable.tf                                                                                    #
 ####################################################################################################
 
-variable "user_display_name" {
-  description = "Guest's display name"
-}
-variable "user_email_address" {
-  description = "Guest's email address"
-}
-variable "message" {
-  description = "Invitation message parameter"
-}
-variable "redirect_url" {
-  description = "Redirect URL for the guest user"
-}
-variable "user_type" {
-  description = "Guest's type of user (Guest or Member)"
+variable "invitation" {
+  description = <<-EOF
+    'var.invitation' is the main variable for azuread_invitation resource settings.
+  EOF
+  type        = object({
+    user_display_name   = string
+    user_email_address  = string
+    redirect_url        = optional(string, "https://microsoft365.com")
+    user_type           = optional(string, null)
+    message             = optional(object({
+      enabled               = optional(bool, true)
+      additional_recipients = optional(list(string), null)  # optional(list(string), [])
+      body                  = optional(string, null)
+      language              = optional(string, null)
+    }), { enabled = false })
+  })
+  validation {
+    condition     = can(length(regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", var.invitation.user_email_address)) > 0)
+    error_message = <<-EOF
+      Value for 'var.invitation.user_email_address' is invalid: ${var.invitation.user_email_address}
+      Must be:
+        RFC 5322 compliant email address
+    EOF
+  }
   validation {
     condition     = (
-      var.user_type == null ? true : contains(["Guest", "Member"], var.user_type)
+      var.invitation.user_type == null ? true : contains(["Guest", "Member"], var.invitation.user_type)
     )
     error_message = <<-EOF
-      Variable 'user_type' has an invalid value: ${var.user_type == null ? 0 : var.user_type}
-      Value must be one of:
+      Value for 'var.invitation.user_type' is invalid: ${var.invitation.user_type == null ? 0 : var.invitation.user_type}
+      Must be one of:
         "Guest", "Member" or null
     EOF
-  } 
+  }
 }
