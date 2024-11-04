f27dd76dfc1796587a64085b5bee7a07c10250a5
Andreas Schreiner <andreas.schreiner@uplink.systems>
2024-11-04 08:54:23 +0100

    updated azurerm_role_definition module

--- modules/azurerm/role_definition/main.tf
+++ modules/azurerm/role_definition/main.tf
@@ -1,16 +1,19 @@
 ####################################################################################################
-#   Azure RM: Custom Role Definitions                                                              # 
+#   main.tf                                                                                        # 
 ####################################################################################################
 
 resource "azurerm_role_definition" "role_definition" {
-  name              = var.name
-  scope             = var.scope
-  assignable_scopes = var.assignable_scopes
-  description       = var.description
-  permissions {
-    actions           = var.permissions.actions
-    data_actions      = var.permissions.data_actions
-    not_actions       = var.permissions.not_actions
-    not_data_actions  = var.permissions.not_data_actions
+  name              = var.role_definition.name
+  scope             = var.role_definition.scope
+  assignable_scopes = var.role_definition.assignable_scopes == [] ? var.role_definition.assignable_scopes : [var.role_definition.scope]
+  description       = var.role_definition.description
+  dynamic "permissions" {
+    for_each            = var.role_definition.permissions.enabled ? [1] : []
+    content {
+      actions             = var.role_definition.permissions.actions
+      data_actions        = var.role_definition.permissions.data_actions
+      not_actions         = var.role_definition.permissions.not_actions
+      not_data_actions    = var.role_definition.permissions.not_data_actions
+    }
   }
 }
--- modules/azurerm/role_definition/variable.tf
+++ modules/azurerm/role_definition/variable.tf
@@ -2,18 +2,21 @@
 #   variables.tf                                                                                   #
 ####################################################################################################
 
-variable "name" {
-  description = "Role's display name"
-}
-variable "description" {
-  description = "Role's description"
-}
-variable "scope" {
-  description = "Role's scoped management group or subscription"
-}
-variable "assignable_scopes" {
-  description = "Role's assignable scoped management groups and/or subscriptions"
-}
-variable "permissions" {
-  description = "Role's permissions"
+variable "role_definition" {
+  description = <<-EOF
+    'var.role_definition' is the main variable for azurerm_role_definition resource settings.
+  EOF
+  type        = object({
+    name                = string
+    description         = optional(string, null)
+    scope               = optional(string, null)
+    assignable_scopes   = optional(list(string), [])
+    permissions         = optional(object({
+      enabled             = optional(bool, true)
+      actions             = optional(list(string), [])
+      data_actions        = optional(list(string), [])
+      not_actions         = optional(list(string), [])
+      not_data_actions    = optional(list(string), [])
+    }), {enabled = false})
+  })
 }
